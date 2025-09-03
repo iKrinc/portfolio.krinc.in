f021b130e76b1c257ad60f3e35cf62b4bdca3f5f
Srinivas Krishna <srinivaskrishna@cartoonmango.com>
2025-09-03 12:27:24 +0530

    INIT COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import Homepage from "./pages/Homepage";
+import './globalStyles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Homepage.css";
+
+function Homepage() {
+  return (
+    <div className="homepage-container">
+      <div className="content">
+        <h1 className="glowing-text">Coming Soon...</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
